65cb325cce0087da43f5ef5b6e50ac39aadf4ff4
sjin9 <sjin9@slb.com>
2023-11-13 01:00:49 +0800

    change path for delete and update

--- ToDoService/Controllers/ToDoItemController.cs
+++ ToDoService/Controllers/ToDoItemController.cs
@@ -56,14 +56,14 @@ namespace ToDoService.Controllers
             return CreatedAtAction(nameof(Get), new { id = toDoItem.Id }, toDoItem);
         }
 
-        [HttpPut]
+        [HttpPut("{id}")]
         public ToDoItem Update(ToDoItem todoItem)
         {
             CheckAndThrowException();
             var foundToDoItem = toDoItems.FirstOrDefault(item => item.Id == todoItem.Id);
             if (foundToDoItem != null)
             {
-                foundToDoItem.IsDone = true;
+                foundToDoItem.IsDone = todoItem.IsDone;
                 foundToDoItem.Description = todoItem.Description;
                 foundToDoItem.Title = todoItem.Title;
             }
@@ -71,7 +71,7 @@ namespace ToDoService.Controllers
             return foundToDoItem;
         }
 
-        [HttpDelete]
+        [HttpDelete("{id}")]
         public ToDoItem Delete(long id)
         {
             CheckAndThrowException();
